8568e5003c3d331780d598de254c8dc53df4f0a2
“GURUVENKAT1996” <gurujoycee96@gmail.com>
2026-04-13 12:13:44 +0530

    new commit

--- main.tf
+++ main.tf
@@ -30,6 +30,7 @@ resource "aws_s3_bucket_public_access_block" "example" {
   restrict_public_buckets = true
 }
 
+
 resource "aws_s3_bucket_versioning" "versioning_example" {
   bucket = aws_s3_bucket.example.id
   versioning_configuration {
